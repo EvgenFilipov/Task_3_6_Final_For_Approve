eb9efb66ecd7117ddb9fa9cc64f02063b685eab3
E.Filipov <filipov62@gmail.com>
2022-06-03 14:44:01 +0300

    Удалил отдельную папку для файла теста

--- test_items.py
+++ test_items.py
@@ -1,4 +1,7 @@
 import time
+
+# Тест проверяет название кнопки Ajouter au panier,
+# Команда запуска: pytest --language=fr test_items.py
 def test_vsyakoy_schlyapi(browser):
     print("\nstart test_for_test")
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/."
@@ -7,9 +10,9 @@ def test_vsyakoy_schlyapi(browser):
     browser.implicitly_wait(10)
 
     searchTextOnTheButton = browser.find_element_by_xpath("//button[@class='btn btn-lg btn-primary btn-add-to-basket']")
-    assert "Añadir al carrito" in searchTextOnTheButton.text, "Текст на кнопке не соответствует ожиданиям!"
+    assert "Ajouter au panier" in searchTextOnTheButton.text, "Текст на кнопке не соответствует ожиданиям!"
 
-    time.sleep(5)
+    time.sleep(30)
 
 
 
